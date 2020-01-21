e15336802c35529938a45eda0f3636e7498a07e0
lmadhuranga <lilan.maduranga@gmail.com>
2020-01-21 23:31:51 +0530

    init

---
+++ routes.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(app) {
+  var UsersController = require('./Controllers/UserController');
+
+  // UsersController Routes
+  app.route('/users')
+    .get(UsersController.list_all_users)
+    .post(UsersController.create_a_user);
+   
+  app.route('/users/:userId')
+  .get(UsersController.read_a_user)
+  .put(UsersController.update_a_user)
+  .delete(UsersController.delete_a_user);
+  };
